574906f69bb1e706af49708d5530c2cef3121c3b
Shehreyar Kitchlew <62876048+Kitchlew@users.noreply.github.com>
2022-02-06 20:16:13 +0500

    Update NFTport.js

--- src/Components/NFTport.js
+++ src/Components/NFTport.js
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 
 export default function NFT() {
   const [nfts, setNFTs] = useState(null)
-
+  const address1="0xD3998D9993B3310C8F739694C97103A5770dbB2E"
     const address="0xa7d8d9ef8d8ce8992df33d8b8cf4aebabd5bd270"
     function PublicAddress(event){
       const text= address.substring(0,6)+". . ."
@@ -20,7 +20,7 @@ export default function NFT() {
       if (nfts !== null) return
         var options = {
           method: 'GET',
-          url: `https://api.nftport.xyz/v0/accounts/${address}`,
+          url: `https://api.nftport.xyz/v0/accounts/${address1}`,
           params: {chain: 'ethereum'},
           headers: {
             'Content-Type': 'application/json',
@@ -44,8 +44,10 @@ export default function NFT() {
 <div style={{margin:"0 auto", fontSize:"40px",padding:"40px",fontWeight:"bolder",color:'white'}}> NFT WALLET</div>
 <div className="Pub_Container" style={{padding:"40px"}}>
   
-  {nfts && nfts.map((file)=> <div ><hr/><div style={{margin:'0 auto',fontWeight:"bolder",padding:"20px",color:"white"}}>{file.name}</div><div style={{padding:"20px"}}><img src={file.file_url} style={{height:"90px",width:"90px",imageRendering: "high-quality",borderRadius:'50%'}}></img></div></div>)}
-   
+  {nfts&& nfts.map((file)=> <div ><hr/><div style={{margin:'0 auto',fontWeight:"bolder",padding:"20px",color:"white"}}>{file.name}</div><div style={{padding:"20px"}}><img src={file.file_url} style={{height:"90px",width:"90px",imageRendering: "high-quality",borderRadius:'50%'}}></img></div></div>)}
+   {nfts===null|| nfts.length===0 &&(
+     <div style={{margin:"0 auto", fontSize:"40px",padding:"40px",fontWeight:"bolder",color:'white'}}>Your Wallet Does not contain any NFTS</div>
+   ) }
  
   </div>
       
